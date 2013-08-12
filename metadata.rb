a721370d47af85c7e47e4681db10f8ee71ad3e10
Rafe Colton <r.colton@modcloth.com>
2013-08-12 16:23:00 -0400

    Adding explicit cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-maintainer       "ModCloth, Inc."
-maintainer_email "ops@modcloth.com"
-license          "Apache 2.0"
-description      "Installs/Configures redis"
+name             'modcloth-redis'
+maintainer       'ModCloth, Inc.'
+maintainer_email 'ops@modcloth.com'
+license          'Apache 2.0'
+description      'Installs/Configures redis'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
-depends "smf"
+depends 'smf'
