64c9ed3f5c1af142496df9275c90242df0bb2ece
Rafe Colton <r.colton@modcloth.com>
2013-09-23 11:57:31 -0400

    Cleaning up some things...
    * "" => ''
    * Apache => MIT
    * switching recipe names
    * new hash syntax

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'modcloth-redis'
 maintainer       'ModCloth, Inc.'
 maintainer_email 'ops@modcloth.com'
-license          'Apache 2.0'
+license          'MIT'
 description      'Installs/Configures redis'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
--- recipes/slave_aof.rb
+++ recipes/slave_aof.rb
@@ -1,3 +1,3 @@
 node.set[:redis][:slaveof] = search(:node, "roles:redis-sessions AND chef_environment:#{node.chef_environment}").first
 
-include_recipe 'modcloth-redis::rails_sessions_aof'
+include_recipe 'modcloth-redis::master_aof'
